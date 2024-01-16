9f9b5551646adbe76d92e775af922a4c8478a69a
Charles Moreno <camoreno1@my.scccd.edu>
2024-01-16 06:47:16 -0800

    coding is done

---
+++ w2/main.js
@@ -0,0 +1,12 @@
+const myValue1 = 4
+const myValue2 = 4
+const myValue3 = 6
+const myValue4 = 2
+const myValue5 = 4
+const myValue6 = 25
+const myValue7 = 4
+const myValue8 = 30
+
+// Do all eight
+
+const total = const myValue1 + const myValue2 + const myValue3 + const myValue4 + const myValue5 + const myValue6 + const myValue7 + const myValue8
