8447852fbc7d451c4deb4c80743e7719181c73f3
Charles Moreno <camoreno1@my.scccd.edu>
2024-02-01 16:53:46 -0800

    updated output to DOM

--- w4/main.js
+++ w4/main.js
@@ -1,7 +1,7 @@
 const cfpData = [];
 
 
-function houseSize(size) {
+function dHSp(houseSize) {
   let houseSizepoints = 0;
   
   if (houseSize === "small") {
@@ -44,14 +44,20 @@ function dHp(numberInHousehold) {
   
   function start(houseHoldmembers, houseSize) {
     const householdPTS = dHp(houseHoldmembers);
-    const houseSizepoints = dHp(houseSize);
+    const houseSizepoints = dHSp(houseSize);
     const total = householdPTS + houseSizepoints;
     cfpData.push([houseHoldmembers, houseSize, householdPTS, houseSizepoints, total]);
     
   }
 
   function displayOutput() {
-
+    for (arr of cfpData){
+      console.log(arr)
+      const output = document.getElementById("output");
+      const newP = document.createElement("p");
+      newP.textContent = `Carbon footprint total is ${arr[4]}, ${arr[1]}, ${arr[2]} is the score from the number of people in the home, ${arr[3]} the score from the size of the home`;
+      output.appendChild(newP);
+    }
   }
 
 start(5, "small")
@@ -60,3 +66,7 @@ start(3, "medium")
 
 
 displayOutput ()
+
+// go back to 7:00 and 11:25 on video to help figure out html issue
+// update template string to reference data memebers of house or size of house
+// i think problem starts on line 53
