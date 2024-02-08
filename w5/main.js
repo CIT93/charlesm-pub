a20c7c00875cc4d47e75b29bbae088747abf2118
Charles Moreno <camoreno1@my.scccd.edu>
2024-02-07 18:47:58 -0800

    setup and array of objects

--- w5/main.js
+++ w5/main.js
@@ -42,34 +42,100 @@ function dHp(numberInHousehold) {
 
   }
   
+
+function displayOutObj(obj) {
+  console.log(obj);
+      const output = document.getElementById("output");
+      const newH2 = document.createElement("h2");
+      newH2.textContent = `Carbon Footprint ${obj.cfpTotal}`;
+      output.appendChild(newH2);
+}
+
   function start(houseHoldmembers, houseSize) {
     const householdPTS = dHp(houseHoldmembers);
     const houseSizepoints = dHSp(houseSize);
     const total = householdPTS + houseSizepoints;
-    cfpData.push([houseHoldmembers, houseSize, householdPTS, houseSizepoints, total]);
+  
+    cfpData.push({
+      houseM: houseHoldmembers,
+      houseS: houseSize,
+      houseMPTS: householdPTS,
+      houseSPTS: houseSizepoints, 
+      cfpTotal: total});
     
+    
+ }
+
+function displayOutput(){
+  for(obj of cfpData) {
+    console.log(obj)
+    const output = document.getElementById("output");
+    const newH2 = document.createElement("h2");
+    newH2.textContent = `Carbon Footprint ${obj.cfpTotal}`;
+    output.appendChild(newH2);
+
   }
 
-  function displayOutput() {
-    for (arr of cfpData){
-      const output = document.getElementById("output");
-      const newH2 = document.createElement("h2");
-      newH2.textContent = `Carbon Footprint ${arr[4]}`;
-      const newH3 = document.createElement("h3");
-      newH3.textContent = `Based on number in and size of home`;
-      const newP = document.createElement("p");
-      newP.textContent = `This number is based on the number of people in the house of ${arr[0]} (score: ${arr[3]},)`;
-      newP.textContent += `and a ${arr[1]} size of home (score:${arr[2]},)`;
-      output.appendChild(newH2);
-      output.appendChild(newH3);
-      output.appendChild(newP);
-    }
-  }
+
+}
+
+  //function displayOutput() {
+   // for (let i = 0; i< cfpData.length; i++){
+      //console.log(i);
+      //const output = document.getElementById("output");
+      //const newH2 = document.createElement("h2");
+      //newH2.textContent = `Carbon Footprint ${cfpData[i][4]}`;
+      //const newH3 = document.createElement("h3");
+      //newH3.textContent = `Based on number in and size of home`;
+      //const newP = document.createElement("p");
+      //newP.textContent = `This number is based on the number of people in the house of ${arr[0]} (score: ${arr[3]}),`;
+      //newP.textContent += `and a ${arr[1]} size of home (score:${arr[2]}).`;
+      //output.appendChild(newH2);
+      //output.appendChild(newH3);
+      //output.appendChild(newP);
+   // }
+  //}
 
 start(5, "small")
 start(4, "large")
 start(3, "medium")
 
+displayOutput()
 
-displayOutput ()
+console.log(cfpData);
 
+// object coding challenge - refactor the start function to store the data in an object
+//displayOutput();
+
+// intro to  object
+//const myArr = [];
+//const myObj = {
+  //cfpTotal: 18,  
+  //houseSize: "small",
+  //displayOut: function (){
+    //console.log("this is a method call")
+    //console.log(this.houseSize);
+  //}
+//};
+//console.log(myObj.cfpTotal);
+//console.log(myObj["houseSize"]);
+//myObj.displayOut()
+
+
+
+//for (let i = 0; i < 5; i ++){
+//block scope
+//console.log(1);
+//}
+// how would we modify this code to count from 1 to 15?
+//for (let i = 1; i <= 15; i ++){
+  //block scope
+  //console.log(1);
+  //}
+  //count backwards
+  //for (let i = 6; i > 0; i --){
+    //block scope
+    //console.log(1);
+    //}
+
+  //refactor for of loop above into standard for; i honestly am not sure how start the refactor. I gave the for portion a try at least. 
